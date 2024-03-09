81f0776884508c74fff15e0c36992111b225e00a
iirlas <bitwisetuna@gmail.com>
2024-03-09 01:12:39 -0600

    Added Score

---
+++ pong/pong.gd
@@ -0,0 +1,36 @@
+extends Level
+
+
+@export var ball_speed := 1.0
+var player_1_score := 0
+var player_2_score := 0
+
+
+func _ready():
+	var angle := snappedf(randf() * 2 * PI, PI / 2.0) + PI / 4.0
+	PhysicsServer2D.body_set_state(
+		$Ball.get_rid(),
+		PhysicsServer2D.BODY_STATE_TRANSFORM,
+		Transform2D.IDENTITY.translated(get_viewport().get_visible_rect().size / 2.0)
+	)
+	PhysicsServer2D.body_set_state(
+		$Ball.get_rid(),
+		PhysicsServer2D.BODY_STATE_LINEAR_VELOCITY,
+		Vector2.RIGHT.rotated(angle) * ball_speed
+	)
+
+
+func _on_goal_body_entered(body):
+	player_2_score += 1
+	_ready()
+
+
+
+func _on_goal_2_body_entered(body):
+	player_1_score += 1
+	_ready()
+
+
+func _process(delta):
+	$UI/Label.text = str(player_1_score)
+	$UI/Label2.text = str(player_2_score)
