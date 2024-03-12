2d69efd8c7e8aa5f5c978d38dd8ef5b1701af9b5
iirlas <bitwisetuna@gmail.com>
2024-03-12 00:36:05 -0500

    Added Page prev and next buttons to level select

--- title_screen/level_select.gd
+++ title_screen/level_select.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var page_offset := 0
+@export var level_offset := 0
 var current_page := 0
 var is_dirty: bool
 
@@ -16,11 +16,11 @@ func _on_select_level(index: int):
 func _process(delta):
 	if not is_dirty:
 		return
-	var level_count: int = SceneManager.scenes.size() - page_offset
+	var level_count: int = SceneManager.scenes.size() - level_offset
 	var button_count := $GridContainer.get_child_count()
 	for i in button_count:
 		var button := $GridContainer.get_child(i) as Button
-		var level_index := i + current_page * button_count + page_offset
+		var level_index := i + current_page * button_count + level_offset
 		for connection in button.pressed.get_connections():
 			button.pressed.disconnect(connection['callable'])
 		if level_index < SceneManager.scenes.size():
@@ -30,3 +30,16 @@ func _process(delta):
 		else:
 			button.visible = false
 	is_dirty = false
+
+
+func _on_button_next_page_pressed():
+	var pages: float = (SceneManager.scenes.size() - level_offset) / float($GridContainer.get_child_count())
+	if (current_page + 1) < pages:
+		current_page += 1
+		is_dirty = true
+
+
+func _on_button_prev_page_pressed():
+	if current_page > 0:
+		current_page -= 1
+		is_dirty = true
